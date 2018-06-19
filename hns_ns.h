82dfbb2ccccdfac279cf1125fcdad0948dc872ac
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-06-19 09:57:41 -0700

    ns: add default nameservers.

--- hns_ns.h
+++ hns_ns.h
@@ -2,7 +2,8 @@
 #define HEADER_HNS_NS_H
 
 static const char *hns_default_ns[] = {
-  "127.0.0.1",
+  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@173.255.248.12",
+  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@45.56.92.136",
   NULL
 };
 
