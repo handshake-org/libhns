cf6989414d4bde5e4a51f56d138a4a1efc1dd96f
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-07-12 07:43:56 -0700

    ns: update servers.

--- hns_ns.h
+++ hns_ns.h
@@ -2,8 +2,8 @@
 #define HEADER_HNS_NS_H
 
 static const char *hns_default_ns[] = {
-  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@173.255.248.12",
-  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@45.56.92.136",
+  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",
+  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",
   NULL
 };
 
