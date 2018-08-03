efde403e4e952f44e07fd821d87a9a0216cfc87d
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-08-02 20:49:00 -0700

    pkg: update libhns.pc.

--- libhns.pc.cmake
+++ libhns.pc.cmake
@@ -1,20 +1,14 @@
-#***************************************************************************
-# Project        ___       __ _ _ __ ___  ___ 
-#               / __|____ / _` | '__/ _ \/ __|
-#              | (_|_____| (_| | | |  __/\__ \
-#               \___|     \__,_|_|  \___||___/
-#
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}/@CMAKE_INSTALL_BINDIR@
 libdir=${prefix}/@CMAKE_INSTALL_LIBDIR@
 includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
 
 Name: hns
-URL: https://c-ares.haxx.se/
-Description: asynchronous DNS lookup library
+URL: https://github.com/handshake-org/libhns
+Description: asynchronous HNS lookup library
 Version: @HNS_VERSION@
-Requires: 
-Requires.private: 
+Requires:
+Requires.private:
 Cflags: -I${includedir} @CPPFLAG_HNS_STATICLIB@
 Libs: -L${libdir} -lhns
 Libs.private: @HNS_PRIVATE_LIBS@
