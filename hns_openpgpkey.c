9b85a1d999cecec8fba9b0c4e45136524abf2778
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-08-02 20:49:16 -0700

    hns: add openpgpkey support.

---
+++ hns_openpgpkey.c
@@ -0,0 +1,49 @@
+/* Copyright 1998 by the Massachusetts Institute of Technology.
+ * Copyright (C) 2009 by Jakub Hrozek <jhrozek@redhat.com>
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
+#include "hns_setup.h"
+#include "hns.h"
+#include "hns_private.h"
+#include "hns_dane.h"
+
+int
+hns_openpgpkey_encode_email(
+  const char *email,
+  char *out,
+  size_t out_len
+) {
+  return hns_dane_encode_email("openpgpkey", email, out, out_len);
+}
+
+int
+hns_openpgpkey_encode_name(
+  const char *name,
+  const char *local,
+  char *out,
+  size_t out_len
+) {
+  return hns_dane_encode_name("openpgpkey", name, local, out, out_len);
+}
+
+size_t
+hns_openpgpkey_email_size(const char *email) {
+  return hns_dane_email_size("openpgpkey", email);
+}
+
+size_t
+hns_openpgpkey_name_size(const char *name) {
+  return hns_dane_name_size("openpgpkey", name);
+}
